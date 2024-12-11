839b37b435c6ebe701277e8829a7f17266cc2fab
nktfmnko <nktfmnko@gmail.com>
2024-12-11 21:23:28 +0300

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+    has_secure_password
+    validates :name, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+  
+    private
+    def password_complexity
+        return if password.blank? || password =~ /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,70}$/
+        errors.add :password, 'Complexity requirement not met. Length should be 8-70 characters and include: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
+    end
+
+    def password_presence
+       errors.add(:password, :blank) unless password_digest.present? 
+    end
+end
